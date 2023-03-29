ab3e5734c0edeca862a86cb7f6f1f798fe306bac
Iuri Torres <iuri.t1000@gmail.com>
2023-03-29 14:56:53 -0300

    test removed cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import cors from 'cors'
+// import cors from 'cors'
 import factory from './factories/userFactory.js'
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 
 export const userController = await factory.initialize()
 
